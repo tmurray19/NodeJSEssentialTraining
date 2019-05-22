ba1a0c13eb8d8774319efeaaca3f3aae162df8c2
tmurray19 <me@taidghmurray.ie>
2019-05-22 17:02:19 +0100

    Slight directory & file restructure.

---
+++ Chapters/2/jsx.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+var style = {
+    backgroundColor: 'LightPink',
+    color: 'white',
+    fontFamily: 'Lato'
+}
+
+ReactDOM.render(
+    <div style = {style}>
+        <h1 id = "heading-element">Hello, world!</h1>
+        <h2>We're glad you're here.</h2>
+    </div>,
+    document.getElementById('root')
+)
