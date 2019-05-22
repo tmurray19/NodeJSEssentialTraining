9d71167db47fed080cef203a3c915e7793b60c6a
tmurray19 <me@taidghmurray.ie>
2019-05-22 16:56:05 +0100

    Refactoring page using JSX

--- react/react-is-fun/src/index.js
+++ react/react-is-fun/src/index.js
@@ -7,24 +7,12 @@ var style = {
     fontFamily: 'Lato'
 }
 
-// Creating an element in React
-// We wish to create a Header for our page
-// We have specified an element called 'title' to be created
-// It is a Header, H1
-// id of 'title'
-// className of 'header'
-// Uses the css style specified above
-// Its child elements consist of a string, 'Hello, world!'
-const title = React.createElement(
-    'h1',
-    {id: 'title', className: 'header', style: style},
-    'Hello, world!'
-)
 
-// This line tells the browser to render the content on the page
-// We render the title into the element 'root' in the HTML
-// (See the index.html file under public)
+
 ReactDOM.render(
-    title,
+    <div style = {style}>
+        <h1 id = "heading-element">Hello, world!</h1>
+        <h2>We're glad you're here.</h2>
+    </div>,
     document.getElementById('root')
 )
